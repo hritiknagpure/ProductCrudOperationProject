f1c07e80fd9599f9fed43830dd53585dc7100429
Hritiknagpure <79217061+hritiknagpure@users.noreply.github.com>
2024-11-01 17:40:13 +0530

    CategoryRepository.java

--- src/main/java/com/hritik/product/repository/CategoryRepository.java
+++ src/main/java/com/hritik/product/repository/CategoryRepository.java
@@ -1,18 +1,10 @@
-
 package com.hritik.product.repository;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.hritik.product.model.Category;
-
-/**
- *
- * @author Java Programming with Aldrin
- */
-
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     
